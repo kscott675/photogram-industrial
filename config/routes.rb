0bdc73df6853956506ad9177f99bdbcd6764d575
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-03-04 23:06:22 +0000

    pass 2points/ route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root "photos#index"
   resources :photos
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "photos#index"
 end
