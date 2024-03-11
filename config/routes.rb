fc2b3a3c57caa5c3c10a8b5f19bdd1e332fb022c
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-03-11 20:55:40 +0000

    added /alice route, created  show template and users controller; yo yo yo!

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root "photos#index"
-  
+
+
   devise_for :users
   
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
+  
+  get "/:username" => "users#show", as: :user
 end
