61279a55da2f96bbab972a5f0fb32af2dd411ba2
Kiowa Scott <97994294+kscott675@users.noreply.github.com>
2024-03-12 15:41:19 +0000

    removed unnecessary files

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,8 +3,9 @@ class PhotosController < ApplicationController
   before_action :set_photo, only: %i[ show edit update destroy ]
 
   # GET /photos or /photos.json
-  def index
-    @photos = Photo.all
+  def feed
+    user = current_user
+    @photos = user.feed
   end
 
   # GET /photos/1 or /photos/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,8 @@ Rails.application.routes.draw do
   resources :photos
   
   get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed"
+  get ":username/followers"
+  get ":username/following"
   get ":username" => "users#show", as: :user
 end
